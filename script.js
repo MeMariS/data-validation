c282e0be8be20fa94b3338e576489af024cd3889
arseniypom <arpomazkov@bk.ru>
2025-05-01 21:46:05 +0800

    Added div example, refactored the form

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const form = document.getElementById("registration");
+const form = document.getElementById('registration');
 
-form.addEventListener("submit", () => {
-  const inputName = document.getElementById("name");
-  const inputEmail = document.getElementById("email");
-  const inputPassword = document.getElementById("password");
-  const inputAge = document.getElementById("age");
+form.addEventListener('submit', (event) => {
+  console.log('Submit event fired!');
+  const inputName = document.getElementById('name');
+  const inputEmail = document.getElementById('email');
+  const inputPassword = document.getElementById('password');
+  const inputAge = document.getElementById('age');
 
   const name = inputName.value;
   const email = inputEmail.value;
@@ -13,41 +14,45 @@ form.addEventListener("submit", () => {
 
   let hasError = false;
 
-  inputName.classList.remove("error");
-  inputEmail.classList.remove("error");
-  inputPassword.classList.remove("error");
-  inputAge.classList.remove("error");
+  inputName.classList.remove('error');
+  inputEmail.classList.remove('error');
+  inputPassword.classList.remove('error');
+  inputAge.classList.remove('error');
 
-  if (name === "") {
-    alert("Имя не должно быть пустым");
-    inputName.classList.add("error");
+  if (name === '') {
+    inputName.classList.add('error');
     hasError = true;
   }
 
-  if (!email.includes("@")) {
-    alert("Email должен содержать @");
-    inputEmail.classList.add("error");
+  if (!email.includes('@')) {
+    inputEmail.classList.add('error');
     hasError = true;
   }
 
   if (password.length < 6) {
-    alert("Пароль должен содержать минимум 6 символов");
-    inputPassword.classList.add("error");
+    inputPassword.classList.add('error');
     hasError = true;
   }
 
-  if (isNaN(age) || age < 0 || age > 120) {
-    alert("Возраст должен быть числом от 0 до 120");
-    inputAge.classList.add("error");
+  if (!age || isNaN(age) || age < 0 || age > 120) {
+    inputAge.classList.add('error');
     hasError = true;
   }
 
   if (!hasError) {
-    alert("Форма успешно отправлена!");
-    console.log("Имя: ", name);
-    console.log("Email: ", email);
-    console.log("Пароль: ", password);
-    console.log("Возраст:, ", age);
-    form.value = "";
+    alert('Форма успешно отправлена!');
+    console.log('Имя: ', name);
+    console.log('Email: ', email);
+    console.log('Пароль: ', password);
+    console.log('Возраст:, ', age);
+    form.value = '';
+  } else {
+    event.preventDefault();
   }
 });
+
+// const parentDiv = document.getElementById('parent');
+
+// parentDiv.addEventListener('click', (event) => {
+//   console.log(event.target.id);
+// });
